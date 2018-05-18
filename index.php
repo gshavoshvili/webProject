e3ea9e08c37e8d80e247f636527c42e2ed2053b8
monstruspro <37114647+monstruspro@users.noreply.github.com>
2018-05-18 14:45:53 +0400

    Link generator done

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
   session_start();
-
   if (!isset($_SESSION['username'])) {
   	$_SESSION['msg'] = "You must log in first";
   	header('location: login.php');
@@ -10,12 +9,30 @@
   	unset($_SESSION['username']);
   	header("location: login.php");
   }
+  //generator
+ if (isset($_GET['action'])) {
+    
+	$keyspace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	$pieces = [];
+	$max = mb_strlen($keyspace, '8bit') - 1;
+	for ($i = 0; $i < 8; ++$i) {
+		$pieces []= $keyspace[random_int(0, $max)];
+	}
+	$_SESSION['link'] = implode('', $pieces);
+	echo $_SESSION['link'];
+	exit();
+ }
+
+ /*if (isset($_SESSION['link'])){
+	 echo $_SESSION['link'];
+ }*/
 ?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Home</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </head>
 <body>
 
@@ -40,9 +57,28 @@
     	<p>Welcome <strong><?php echo $_SESSION['username']; ?></strong></p>
     	<p> <a href="index.php?logout='1'" style="color: red;">logout</a> </p>
     <?php endif ?>
-	<input type="button" value="Generate link">
-	<p id="uid"></p>
-	<p>Send this link to friend of yours</p>
+
+	<input type="submit" class="button" name="generate" value="Generate link">
+
+	<script> 
+		
+	$('.button').click(function() {
+
+		$.ajax({
+		type: "GET",
+		url: "index.php",
+		data: { action: "abc" }
+		}).done(function(msg) {
+		alert( "Data Saved: " + msg );
+		window.location.href = "game.php";
+		});
+  	 });
+
+	
+	</script>
+	<div id="uid" style="display:none">
+	<p></p>
+	</div>
 </div>
 
 </body>
