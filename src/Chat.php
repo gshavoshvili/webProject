f55583aea33ff145c1e11f33ef534d2be3fb4165
monstruspro <37114647+monstruspro@users.noreply.github.com>
2018-05-25 19:35:08 +0400

    ыыы

--- src/Chat.php
+++ src/Chat.php
@@ -1,5 +1,6 @@
 <?php
 namespace MyApp;
+include "MatchClass.php";
 use Ratchet\MessageComponentInterface;
 use Ratchet\ConnectionInterface;
 
@@ -7,7 +8,6 @@ class Chat implements MessageComponentInterface {
     protected $connections;
 
     public function __construct() {
-        
         $this->connections = array();
     }
 
@@ -15,25 +15,49 @@ class Chat implements MessageComponentInterface {
         // Store the new connection to send messages to later
         $this->connections[]=$conn;
         echo "New connection! ({$conn->resourceId})\n";
-        if (count($this->connections)==2){
+       /* 
+            if (count($this->connections)==2){
             $first = rand(0,1);
             $this->connections[$first]->send("START");
             echo "Game started: player $first is first";
         }
-
+       */
         
     }
 
-    public function onMessage(ConnectionInterface $from, $msg) {
+    public function onMessage(ConnectionInterface $from, $jsmessage) {
+        $db = mysqli_connect('localhost', 'root', '', 'registration');
+        $json = json_decode($jsmessage);
+        $unsafe_username = $json->username;
+        $unsafe_match_link = $json->match;
+        $username = mysqli_real_escape_string($db,$unsafe_username);
+        $match_link = mysqli_real_escape_string($db,$unsafe_username);
+        $match = new Match;
+
+        if(isset($username) && isset($match_link)){
+            
+            $match_link_check_query = "SELECT match_link from matches where match_link = '$match_link' AND (creator_id = (select id from users where username = '$username') OR opponent_id = (select id from users where username = '$username')) LIMIT 1";
+            $result = mysqli_query($db, $match_link_check_query);
+            $match_link = mysqli_fetch_assoc($result);
+            if(isset($match_link)){
+                
+                $match->FirstPlayerReady();
+
+            }
+            
+        }
+        
+        
         $numRecv = count($this->connections) - 1;
         echo sprintf('Connection %d sending message "%s" to %d other connection%s' . "\n"
-            , $from->resourceId, $msg, $numRecv, $numRecv == 1 ? '' : 's');
+            , $from->resourceId, $jsmessage, $numRecv, $numRecv == 1 ? '' : 's');
 
         foreach ($this->connections as $client) {
             if ($from !== $client) {
                 // The sender is not the receiver, send to each client connected
-                $client->send($msg);
+                $client->send($jsmessage);
             }
+        
         }
     }
 
---
+++ src/MatchClass.php
@@ -0,0 +1,43 @@
+<?php
+namespace MyApp;
+class Match{
+public $player1;
+public $player2;
+public $array1 = array();
+public $array2 = array();
+public $status;
+
+    public function FirstPlayer($username){
+        
+        $this->$player1=$username;
+
+    }
+
+    public function SecondPlayer($username){
+        
+        $this->$player2=$username;
+
+    }
+
+    public function FirstPlayerFuild(){
+
+    }
+
+    public function SecondPlayerFuild(){
+
+    }
+    
+    public function GameStatus(){
+
+    }
+
+    public function Connection1(){
+
+    }
+    
+    public function Connection2 (){
+
+    }
+
+}
+?>    
